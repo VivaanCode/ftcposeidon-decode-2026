b4e8b74ddeb9ca01636fd5f2e11ce993eb495e7d
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-01-10 12:08:53 -0800

    moved april tag webcam to subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 // import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
+import org.firstinspires.ftc.teamcode.subsystems.AprilTagWebcam;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 
