49fbb4fdb41779a0cf37b5551496cf9def9610e6
l1ionelw <143676566+l1ionelw@users.noreply.github.com>
2026-01-16 18:22:32 -0800

    teleop done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopMain.java
@@ -9,7 +9,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.Objects;
-import java.util.Timer;
+
+// spindexer
 
 @TeleOp(name = "Teleop Main")
 public class TeleopMain extends LinearOpMode {
@@ -22,9 +23,12 @@ public class TeleopMain extends LinearOpMode {
 
     double KICKER_MAX_POSITION = 0.95;
     double KICKER_MIN_POSITION = 0;
+    double SPINDEXER_ONE_ROTATION = 0;
+    double SPINDEXER_POSITION = 0;
 
     // STATES
     double KICKER_LOWER_TIMER = -1;
+    boolean KICKER_LOWER_TIMER_SET = false;
     String SHOOTER_STATE = "IDLE"; // IDLE, SPINUP, READY
     String KICKER_STATE = "IDLE"; // IDLE, MOVE_UP, WAITING, MOVE_DOWN, LOAD_TO_SHOOTER
 
@@ -32,6 +36,7 @@ public class TeleopMain extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        SPINDEXER_POSITION = spindexer.getPosition();
         initMotors();
         timer.reset();
         waitForStart();
@@ -47,7 +52,32 @@ public class TeleopMain extends LinearOpMode {
             kicker2.setPosition(KICKER_MAX_POSITION);
         }
 
-        // KICKER STATE, fix this to work with turret
+        if (gamepad2.dpadLeftWasPressed()) {
+            SPINDEXER_POSITION -= SPINDEXER_ONE_ROTATION / 3;
+        }
+        if (gamepad2.dpadRightWasPressed()) {
+            SPINDEXER_POSITION += SPINDEXER_ONE_ROTATION / 3;
+        }
+
+        if (gamepad2.x) {
+            // turn everything off
+        }
+        if (gamepad2.a) {
+            // spin up everything - works
+            KICKER_STATE = "MOVE_UP";
+            SHOOTER_STATE = "SPINUP";
+        }
+        if (gamepad2.y) {
+            // immediately shoot
+            KICKER_STATE = "LOAD_TO_SHOOTER";
+            SHOOTER_STATE = "READY";
+        }
+
+        // KICKER STATE
+        if (Objects.equals(KICKER_STATE, "MOVE_DOWN")) {
+            kicker1.setPosition(KICKER_MIN_POSITION);
+            kicker2.setPosition(KICKER_MIN_POSITION);
+        }
         if (Objects.equals(KICKER_STATE, "MOVE_UP")) {
             kicker1.setPosition(0.65);
             kicker2.setPosition(0.65);
@@ -55,23 +85,23 @@ public class TeleopMain extends LinearOpMode {
         if (Objects.equals(KICKER_STATE, "MOVE_UP") && kicker1.getPosition() > 0.6) {
             KICKER_STATE = "WAITING";
         }
-        if (Objects.equals(SHOOTER_STATE, "READY")) {
-            // shooter ready! kickers up!
-            KICKER_STATE = "LOAD_TO_SHOOTER";
-        }
         if (Objects.equals(SHOOTER_STATE, "LOAD_TO_SHOOTER")) {
             kicker1.setPosition(KICKER_MAX_POSITION);
             kicker2.setPosition(KICKER_MAX_POSITION);
         }
         if (Objects.equals(KICKER_STATE, "LOAD_TO_SHOOTER") && kicker1.getPosition() > 0.9) {
             // stop moving, but wait 1 second before moving back down
-            KICKER_LOWER_TIMER = timer.milliseconds() + 1000;
+            if (!KICKER_LOWER_TIMER_SET) {
+                KICKER_LOWER_TIMER = timer.milliseconds() + 1000;
+                KICKER_LOWER_TIMER_SET = true;
+            }
             kicker1.setPosition(kicker1.getPosition());
             kicker2.setPosition(kicker2.getPosition());
         }
         if (Objects.equals(KICKER_STATE, "LOAD_TO_SHOOTER") && timer.milliseconds() > KICKER_LOWER_TIMER) {
             SHOOTER_STATE = "IDLE";
             KICKER_STATE = "MOVE_DOWN";
+            KICKER_LOWER_TIMER_SET = false;
             kicker1.setPosition(KICKER_MIN_POSITION);
             kicker2.setPosition(KICKER_MIN_POSITION);
         }
@@ -80,11 +110,17 @@ public class TeleopMain extends LinearOpMode {
         if (Objects.equals(SHOOTER_STATE, "IDLE")) {
             shooterMotor.setPower(0);
         }
-        if (Objects.equals(SHOOTER_STATE, "SPINUP") && shooterMotor.getVelocity() > 100) {
+        if (Objects.equals(SHOOTER_STATE, "SPIN_UP")) {
+            shooterMotor.setPower(1);
+        }
+        if (Objects.equals(SHOOTER_STATE, "SPINUP") && shooterMotor.getVelocity() > 100 && Objects.equals(KICKER_STATE, "WAITING")) {
             // ready now, start shooting
             SHOOTER_STATE = "READY";
             KICKER_STATE = "LOAD_TO_SHOOTER";
         }
+
+        // spindexer
+        spindexer.setPosition(SPINDEXER_POSITION);
 
         // move the robot
         moveRobot(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
